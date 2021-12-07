7ca5282dbb2fd689bf1565dc81aea2d154359356
amineohn <amineprojet7@gmail.com>
2021-12-07 15:31:51 +0100

    migrate to typescript :)

--- index.js
+++ index.js
@@ -1,67 +1,106 @@
-const Mustache = require("mustache");
-const fs = require("fs");
-const MUSTACHE_MAIN_DIR = "./main.mustache";
-
-async function isToday(dateParameter) {
-  const today = new Date();
-  return (
-    dateParameter.getDate() === today.getDate() &&
-    dateParameter.getMonth() === today.getMonth()
-  );
-}
-
-let special = "";
-
-switch (special) {
-  case isToday(new Date("01-01-2000")):
-    special = "Happy New Year to me !!!";
-    break;
-  case isToday(new Date("02-21-2000")):
-    special = "Happy Birthday to my mother !!!";
-    break;
-  case isToday(new Date("03-01-2000")):
-    special = "Happy Birthday to my father !!!";
-    break;
-  case isToday(new Date("06-03-2000")):
-    special = "Happy Birthday to my brother !!!";
-    break;
-  case isToday(new Date("11-07-2000")):
-    special = "Happy Birthday to Tam-sensei !!!";
-    break;
-  case isToday(new Date("08-02-2000")):
-    special = "Happy Birthday to Trs !!!";
-    break;
-  case isToday(new Date("01-12-2000")):
-    special = "Happy Birthday to Buu !!!";
-    break;
-  case isToday(new Date("11-24-2000")):
-    special = "Happy Birthday to Cat !!!";
-    break;
-  case isToday(new Date("02-19-2000")):
-    special = "Happy Birthday to you !!!";
-    break;
-  default:
-    special = "";
-}
-
-let DATA = {
-  special: special,
-  name: "Ho Xuan Vinh",
-  date: new Date().toLocaleDateString("en-FR", {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    timeZoneName: "short",
-    timeZone: "Europe/Stockholm",
-  }),
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var Mustache = require("mustache");
+var fs = require("fs");
+var MUSTACHE_MAIN_DIR = "./main.mustache";
+function isToday(dateParameter) {
+    return __awaiter(this, void 0, void 0, function () {
+        var today;
+        return __generator(this, function (_a) {
+            today = new Date();
+            return [2 /*return*/, (dateParameter.getDate() === today.getDate() &&
+                    dateParameter.getMonth() === today.getMonth())];
+        });
+    });
+}
+var special = "";
+switch (special) {
+    case isToday(new Date("01-01-2000")):
+        special = "Happy New Year to me !!!";
+        break;
+    case isToday(new Date("02-21-2000")):
+        special = "Happy Birthday to my mother !!!";
+        break;
+    case isToday(new Date("03-01-2000")):
+        special = "Happy Birthday to my father !!!";
+        break;
+    case isToday(new Date("06-03-2000")):
+        special = "Happy Birthday to my brother !!!";
+        break;
+    case isToday(new Date("11-07-2000")):
+        special = "Happy Birthday to Tam-sensei !!!";
+        break;
+    case isToday(new Date("08-02-2000")):
+        special = "Happy Birthday to Trs !!!";
+        break;
+    case isToday(new Date("01-12-2000")):
+        special = "Happy Birthday to Buu !!!";
+        break;
+    case isToday(new Date("11-24-2000")):
+        special = "Happy Birthday to Cat !!!";
+        break;
+    case isToday(new Date("02-19-2000")):
+        special = "Happy Birthday to you !!!";
+        break;
+    default:
+        special = "";
+}
+var DATA = {
+    special: special,
+    name: "Ho Xuan Vinh",
+    date: new Date().toLocaleDateString("en-FR", {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeZoneName: "short",
+        timeZone: "Europe/Stockholm"
+    })
+};
+console.log(DATA);
 function action() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
-    if (err) throw err;
-    const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync("README.md", output);
-  });
+    fs.readFile(MUSTACHE_MAIN_DIR, function (err, data) {
+        if (err)
+            throw err;
+        var output = Mustache === null || Mustache === void 0 ? void 0 : Mustache.render(data.toString(), DATA);
+        fs === null || fs === void 0 ? void 0 : fs.writeFileSync("README.md", output);
+    });
 }
 action();
---
+++ index.ts
@@ -0,0 +1,68 @@
+import Mustache = require("mustache");
+import fs = require("fs");
+const MUSTACHE_MAIN_DIR = "./main.mustache";
+
+async function isToday(dateParameter: any): Promise<boolean> {
+  const today = new Date();
+  return (
+    dateParameter.getDate() === today.getDate() &&
+    dateParameter.getMonth() === today.getMonth()
+  );
+}
+
+let special = "";
+
+switch (special) {
+  case isToday(new Date("01-01-2000")) as any:
+    special = "Happy New Year to me !!!";
+    break;
+  case isToday(new Date("02-21-2000")) as any:
+    special = "Happy Birthday to my mother !!!";
+    break;
+  case isToday(new Date("03-01-2000")) as any:
+    special = "Happy Birthday to my father !!!";
+    break;
+  case isToday(new Date("06-03-2000")) as any:
+    special = "Happy Birthday to my brother !!!";
+    break;
+  case isToday(new Date("11-07-2000")) as any:
+    special = "Happy Birthday to Tam-sensei !!!";
+    break;
+  case isToday(new Date("08-02-2000")) as any:
+    special = "Happy Birthday to Trs !!!";
+    break;
+  case isToday(new Date("01-12-2000")) as any:
+    special = "Happy Birthday to Buu !!!";
+    break;
+  case isToday(new Date("11-24-2000")) as any:
+    special = "Happy Birthday to Cat !!!";
+    break;
+  case isToday(new Date("02-19-2000")) as any:
+    special = "Happy Birthday to you !!!";
+    break;
+  default:
+    special = "";
+}
+
+let DATA = {
+  special: special,
+  name: "Ho Xuan Vinh",
+  date: new Date().toLocaleDateString("en-FR", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    timeZoneName: "short",
+    timeZone: "Europe/Stockholm",
+  }),
+};
+console.log(DATA);
+function action() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err: any, data: any) => {
+    if (err) throw err;
+    const output = Mustache?.render(data.toString(), DATA);
+    fs?.writeFileSync("README.md", output as string);
+  });
+}
+action();
